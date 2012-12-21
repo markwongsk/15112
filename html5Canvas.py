e13f956ac4323f9237b27a8cce9dfc1ecd461061
Mark Wong Siang Kai <markwongsk@gmail.com>
2012-12-21 08:02:15 -0500

    Just dabbling around with some stuff

---
+++ html5Canvas.py
@@ -0,0 +1,13 @@
+# @file html5Canvas.py
+# @brief implementation to display html5 canvas in a webbrowser using python 
+# calls
+# @author Mark Wong Siang Kai <markwongsk@gmail.com>
+
+class HTML5Canvas:
+
+  def __init__(width,height):
+    self.width = width
+    self.height = height
+
+  def create_rectangle(left, top, right, bottom):
+    return 1/0;
